1016e9bbf2c49ce0edb658d7fb093fae8962f522
mdelvecchio <mdelvecchio3119@gmail.com>
2023-08-05 14:05:53 -0400

    CRUD complete

---
+++ src/pages/Show.js
@@ -0,0 +1,27 @@
+import { useLoaderData, Form } from "react-router-dom"
+
+function Show(props) {
+	const person = useLoaderData()
+	
+	return (
+		<div className="person">
+			<h1>{person.name}</h1>
+			<h2>{person.title}</h2>
+			<img src={person.image} alt={person.name}/>
+
+		<h2>Update: {person.name}</h2>
+		<Form action={`/update/${person._id}`} method='post'>
+			<input type='text' name='name' placeholder="person's name" defaultValue={person.name}/>
+			<input type='text' name='title' placeholder="person's title" defaultValue={person.title}/>
+			<input type='text' name='image' placeholder="person's image" defaultValue={person.image}/>
+			<input type='submit' value={`Update ${person.name}`} />
+		</Form>
+		<h2>Delete {person.name}</h2>
+		<Form action={`/delete/${person._id}`} method="post">
+			<input type="submit" value={`Delete ${person.name}`}/>
+		</Form>
+		</div>
+	)
+}
+
+export default Show;
